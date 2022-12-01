06fa5b5950f1a54d4b7e23d9fa1dc81bd6173e20
teebee09 <tijaniteebee09@gmail.com>
2022-12-01 00:07:06 -0800

    Update If Else Loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for digits in range(0, 10):
     for digits2 in range(digits + 1, 10):
-        if digits == 8 and digits2 == 9:
-           print("{}{}".format(digits, digits2)
-        else:
+        if digits != 8:
            print("{}{}".format(digits, digits2), end=", ")
+print("89")
