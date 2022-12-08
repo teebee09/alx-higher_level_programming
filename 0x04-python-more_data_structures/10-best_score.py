88655d2021133cc1192dc47a26fa671e1a20ff77
teebee09 <tijaniteebee09@gmail.com>
2022-12-08 09:03:02 -0800

    python more data structure

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    highest_key = 0
-    highest_value = None
+    highest_value = 0
+    highest_key = None
     if a_dictionary:
         for key, value in a_dictionary.items():
             if value > biggest_value:
