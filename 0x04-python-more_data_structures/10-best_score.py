679a5a2cba8d9c656c1d57111563e10354172ec5
teebee09 <tijaniteebee09@gmail.com>
2022-12-08 09:04:52 -0800

    python more data structure

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     highest_key = None
     if a_dictionary:
         for key, value in a_dictionary.items():
-            if value > biggest_value:
+            if value > highest_value:
                 highest_value = value
                 highest_key = key
     return highest_key
