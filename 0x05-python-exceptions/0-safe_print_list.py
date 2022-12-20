231355b7f45b9444b1449fff4e85ed2d147f940d
teebee09 <tijaniteebee09@gmail.com>
2022-12-19 19:28:21 -0800

    python exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             pichu += 1
-        except:
-            continue
-    print()
+        except IndexError:
+            break
+    print("")
     return pichu
