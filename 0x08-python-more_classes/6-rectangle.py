842e990d1ea1f8c1ae12ba8a70cb3eb62ad022a8
Tijani Oluwatobiloba <tijaniteebee09@gmail.com>
2023-01-04 06:07:31 +0100

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """Represent a rectangle.
+
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
     """
@@ -12,6 +13,7 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
+
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -58,6 +60,7 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
+
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
