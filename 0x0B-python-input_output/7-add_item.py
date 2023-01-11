999da20d47c0f5ca89b3d5f8dbbc153a3194f82f
Tijani Oluwatobiloba <tijaniteebee09@gmail.com>
2023-01-11 04:47:49 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""
-Module 7-save_to_json_file
-
-Contains function that writes Python obj to file using JSON represenation
-"""
+# 7-save_to_json_file.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a JSON file-writing function."""
+import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Writes Python obj to file using JSON represenation
-    Args:
-        my_obj: python object
-        filename: file
-    """
-    import json
-
-    with open(filename, mode="w", encoding="utf-8") as f:
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
         json.dump(my_obj, f)
