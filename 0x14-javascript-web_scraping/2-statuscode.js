111ab5dadd97afa02c774ac692326d86e6301e08
Tijani Oluwatobiloba <tijaniteebee09@gmail.com>
2023-04-26 12:44:10 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-
-let url = process.argv[2];
+/* displays the status code of a GET request.
+    - the first argument is the URL to request (GET).
+    - output format: 'code: <status code>'.
+*/
 const request = require('request');
-
-request(url, function (err, response) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code: ' + response.statusCode);
-  }
+request(process.argv[2], function (error, response) {
+  if (error) console.log(error);
+  else console.log('code: ' + response.statusCode);
 });
